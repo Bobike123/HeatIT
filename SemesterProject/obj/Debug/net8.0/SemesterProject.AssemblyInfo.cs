6fb4a861084b72aafa508e41570c904eb8a52e4b
RadekChy25 <chylak.radoslaw@gmail.com>
2024-04-12 11:54:39 +0200

    ProductionValues update

--- SemesterProject/obj/Debug/net8.0/SemesterProject.AssemblyInfo.cs
+++ SemesterProject/obj/Debug/net8.0/SemesterProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SemesterProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7eaaaea1f941c8b5388ab1b885a9bd712e6deaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b7f69f1799bfdb84c085efb576a337a603c8fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SemesterProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SemesterProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
