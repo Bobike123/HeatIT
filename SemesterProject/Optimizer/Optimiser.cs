681abf21d1dea8be155308700ad4c73106cc3801
RadekChy25 <chylak.radoslaw@gmail.com>
2024-04-30 17:15:05 +0200

    Optimiser Change#1

--- SemesterProject/Optimizer/Optimiser.cs
+++ SemesterProject/Optimizer/Optimiser.cs
@@ -1,12 +1,47 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
-namespace SemesterProject.Views
-{
+namespace SemesterProject;
     public class Optimiser
     {
-        
+        public void Optimization()
+        {
+            var firstSc = ReadFile.productionUnits.OrderBy(unit => ChangeToDouble(unit.ProductionCosts!)).ToList();
+
+            var secondSc = ReadFile.productionUnits.OrderBy(unit => 
+            {
+                double netProductionCost;
+                if (ChangeToDouble(unit.MaxElectricity!) > 0)
+                {
+                    netProductionCost = ChangeToDouble(unit.ProductionCosts!) - (ChangeToDouble(unit.MaxElectricity!) * GetCurrentElectricityPrice());
+                }
+                else if(ChangeToDouble(unit.MaxElectricity!) < 0)
+                {
+                    netProductionCost = ChangeToDouble(unit.ProductionCosts!) + (Math.Abs(ChangeToDouble(unit.MaxElectricity!)) * GetCurrentElectricityPrice());
+                }
+                else
+                {
+                    netProductionCost = ChangeToDouble(unit.ProductionCosts!);
+                }
+                return netProductionCost;
+            }).ToList();
+        }
+        private double GetCurrentElectricityPrice()
+        {
+            //Implement logic to get current electricity price based on the time.
+            return 100.00;
+        }
+        private static double ChangeToDouble(string value)
+        {
+            try
+            {
+                double doubleValue = double.Parse(value);
+                return doubleValue;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Cannot convert: {ex}");
+                return 0;
+            }
+        }
     }
-}
