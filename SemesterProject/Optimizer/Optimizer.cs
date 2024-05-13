5e32fe90e5efe815dc9deed804f80f5dde8c8af7
RadekChy25 <chylak.radoslaw@gmail.com>
2024-05-13 13:32:00 +0200

    Optimizer Update

--- SemesterProject/Optimizer/Optimizer.cs
+++ SemesterProject/Optimizer/Optimizer.cs
@@ -1,24 +1,24 @@
-using System;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using SemesterProject.Views;
 
 namespace SemesterProject;
 public class Optimizer
 {
-    public static void Calculation(string[][] olas)
+    public static double[][] Calculation(string period, string[][] olas)
     {
-        //double[][] netProduction = new double[olas.Length][];
-        //double[] array = new double[ReadFile.productionUnits.Length];
-        string[][] unitsSUMMER = new string[olas.Length][];
-        string[][] unitsWINTER = new string[olas.Length][];
+        double[][] unitsSUMMER = new double[olas.Length][];
+        double[][] unitsWINTER = new double[olas.Length][];
 
         // Initialize each inner array
         for (int i = 0; i < unitsSUMMER.Length; i++)
         {
-            unitsSUMMER[i] = new string[4];
-            unitsWINTER[i] = new string[4];
+            unitsSUMMER[i] = new double[4];
+            unitsWINTER[i] = new double[4];
 
         }
 
-        for (int i = 0; i < ReadFile.productionUnits.Length; i++)
+        for (int i = 0; i < ReadFile.productionUnits.Count; i++)
         {
             for (int j = 0; j < olas.Length; ++j)
             {
@@ -26,32 +26,47 @@ public class Optimizer
                 double c = ChangeToDouble(ReadFile.productionUnits[i].ProductionCosts!);
                 if (v > 0)
                 {
-                    unitsSUMMER[j][i] = (c - (v * ChangeToDouble(olas[j][1]))).ToString("00.00");
-                    unitsWINTER[j][i] = (c - (v * ChangeToDouble(olas[j][0]))).ToString("00.00");
+                    unitsSUMMER[j][i] = c - (v * ChangeToDouble(olas[j][1]));
+                    unitsWINTER[j][i] = c - (v * ChangeToDouble(olas[j][0]));
                 }
                 else if (v < 0)
                 {
-                    unitsSUMMER[j][i] = (c + (Math.Abs(v) * ChangeToDouble(olas[j][1]))).ToString("00.00");
-                    unitsWINTER[j][i] = (c + (Math.Abs(v) * ChangeToDouble(olas[j][0]))).ToString("00.00");
+                    unitsSUMMER[j][i] = c + (Math.Abs(v) * ChangeToDouble(olas[j][1]));
+                    unitsWINTER[j][i] = c + (Math.Abs(v) * ChangeToDouble(olas[j][0]));
                 }
                 else
                 {
-                    unitsSUMMER[j][i] = c.ToString();
-                    unitsWINTER[j][i] = c.ToString();
+                    unitsSUMMER[j][i] = c;
+                    unitsWINTER[j][i] = c;
                 }
 
             }
         }
+        for(int i = 0; i< unitsSUMMER.Length; ++i)
+        {
+            Array.Sort(unitsSUMMER[i],  new Comparison<double>( 
+            (x,y) => { return x < y ? -1 : (x > y ? 1 : 0); }
+            ));
+        }
+        
+        for(int i = 0; i< unitsWINTER.Length; ++i)
+        {
+            Array.Sort(unitsWINTER[i],  new Comparison<double>( 
+            (x,y) => { return x < y ? -1 : (x > y ? 1 : 0); }
+            ));
+        }
 
+        string[][] orderedSummer = Convert(unitsSUMMER);
+        string[][] orderedWinter = Convert(unitsWINTER);
         // Do something with 'units' array, such as writing to a CSV file
-        ResultDataManager.AppendToCSV("newFile.csv", unitsWINTER);
+        ResultDataManager.AppendToCSV("newFile.csv", orderedWinter);
         string[][] empty = {
             [""],
             [""]
         };
         ResultDataManager.AppendToCSV("newFile.csv", empty);
 
-        ResultDataManager.AppendToCSV("newFile.csv", unitsSUMMER);
+        ResultDataManager.AppendToCSV("newFile.csv", orderedSummer);
 
         double[] sumWinter = new double[unitsWINTER[0].Length];
         double[] sumsummer = new double[unitsSUMMER[0].Length];
@@ -60,8 +75,8 @@ public class Optimizer
         {
             for (int j = 0; j < unitsWINTER[i].Length; j++)
             {
-                sumWinter[j] += ChangeToDouble(unitsWINTER[i][j]);
-                sumsummer[j] += ChangeToDouble(unitsSUMMER[i][j]);
+                sumWinter[j] += unitsWINTER[i][j];
+                sumsummer[j] += unitsSUMMER[i][j];
 
             }
         }
@@ -70,8 +85,15 @@ public class Optimizer
             Console.WriteLine($"Winter for the {i} unit {sumWinter[i]:00.00}");
             Console.WriteLine($"Summer for the {i} unit {sumsummer[i]:00.00}");
         }
+        if(period == "winter")
+        {
+            return unitsWINTER;
+        }
+        else
+        {
+            return unitsSUMMER;
+        }
     }
-
     public static double ChangeToDouble(string value)
     {
         try
@@ -84,5 +106,35 @@ public class Optimizer
             Console.WriteLine($"Cannot convert: {ex}");
             return 0;
         }
+    }
+    public static string[][] Convert(double[][] values)
+    {
+        string[][] stringArray = new string[values.Length][];
+        for(int i = 0; i < values.Length; ++i)
+        {
+            stringArray[i] = new string[values[i].Length];
+            for(int j = 0; j < values[i].Length; ++j)
+            {
+                stringArray[i][j] = values[i][j].ToString();
+            }
+        }
+        return stringArray;
+    }
+    public static void OperatingPoint(string[][] data, double[][] cost, int period)
+    {
+        double[][] skies = new double[data.Length][];
+        double[][] operatingPoint = new double[data.Length][];
+        for(int i = 0; i < data.Length; ++i)
+        {
+            skies[i] =  new double[ReadFile.productionUnits.Count];
+            operatingPoint[i] =  new double[ReadFile.productionUnits.Count];
+            for(int j = 0; j < ReadFile.productionUnits.Count; ++j)
+            {
+                operatingPoint[i][j] = ChangeToDouble(data[i][period]) / ChangeToDouble(ReadFile.productionUnits[j].MaxHeat!);
+            }
+        }
+        
+        string[][] operatingPointString = Convert(operatingPoint);
+        ResultDataManager.AppendToCSV("operatingPoint.csv", operatingPointString);
     }
 }
